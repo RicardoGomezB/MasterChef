31bc5805ae80d379e11fd171678b30d0454e93c5
Jose Antonio Alguindigue <jose.alguindigue@correo.unimet.edu.ve>
2019-03-01 18:22:12 -0400

    typescript para a base de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlatoDetalleComponent } from './componentes/plato-detalle/plato-detalle.component';
+import { DataService } from './services/data.service';
+import { DataDetalleService } from './services/data-detalle.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatoDetalleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService, DataDetalleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
